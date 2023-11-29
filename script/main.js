3dfd74a236e3cde931daf081a9c8a2a18d9334ac
Antonio Guerrero <antonioguerrero2109@gmail.com>
2023-11-29 01:38:18 -0600

    Funciones extra añadidas

--- script/main.js
+++ script/main.js
@@ -1,34 +1,35 @@
-const readNumbers = (value) =>{
-    document.getElementById("resultText").value+=value;
-}
-const calculate = () =>{
-    let result = document.getElementById("resultText");
+const result = document.getElementById("resultText");
+
+const readNumbers = value => result.value += value;
+
+const calculate = () => {
     try {
         result.value = eval(result.value);
         if (result.value == "Infinity") {
-            result.value = "ERROR";
-            setTimeout(() => {
-                clean();
-            }, 3000);
+            displayError();
         }
     } catch (error) {
-        result.value = "ERROR";
-        setTimeout(() => {
-            clean();
-        }, 3000);
+        displayError();
     }
 }
-const clean = () =>{
-    let result = document.getElementById("resultText");
-    result.value = "";
+
+const pow2 = () => result.value = Math.pow(result.value, 2);
+
+const squareRoot = () => result.value = Math.sqrt(result.value);
+
+const clean = () => result.value = "";
+
+const change = () => {
+    if (result.value !== "") {
+        result.value = result.value.startsWith('-') ? result.value.slice(1) : '-' + result.value;
+    }
 }
-const change = () =>{
-    let result = document.getElementById("resultText");
-        if (result.value !== "") {
-            result.value = result.value.startsWith('-') ? result.value.slice(1) : '-' + result.value;
-        }
-}
-const deleteChar = () =>{
-    let result = document.getElementById("resultText");
-    result.value = result.value.slice(0,-1);
+
+const deleteChar = () => result.value = result.value.slice(0, -1);
+
+const copyResult = () => navigator.clipboard.writeText(result.value);
+
+const displayError = () => {
+    result.value = "ERROR";
+    setTimeout(clean, 3000);
 }
